f3c07fe2dab0a37421bbb108258da2ccf41db579
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-07-11 18:13:05 +0300

    setup the server

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected Successfully: ${socket.id}`);
+});
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
