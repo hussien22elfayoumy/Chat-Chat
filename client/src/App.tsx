8e1986b7d2fa2b73fa0d5f994437e75de97a88d2
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-07-11 20:46:37 +0300

    feat: implement chat design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,21 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 const socket = io("http://localhost:8080");
 
+type MessageData = {
+  message: string;
+  userName: string;
+  roomId: string;
+  time: string;
+};
+
+let flag = 0;
+
 function App() {
   const [roomId, setRoomId] = useState("");
   const [userName, setUserName] = useState("");
   const [message, setMessage] = useState("");
   const [showMessages, setShowMessages] = useState(false);
+  const [messagesList, setMessagesList] = useState<MessageData[]>([]);
 
   const hanldeJoinRoom = () => {
     if (!roomId || !userName) return;
@@ -30,12 +40,15 @@ function App() {
     };
 
     socket.emit("send-message", messageData);
+    setMessagesList((prevList) => [...prevList, messageData]);
+
     setMessage("");
   };
 
   useEffect(() => {
     socket.on("receive-message", (messageData) => {
       console.log(messageData);
+      setMessagesList((prevList) => [...prevList, messageData]);
     });
   }, [socket]);
 
@@ -85,7 +98,37 @@ function App() {
         </section>
       ) : (
         <section className="flex h-[70vh] w-full max-w-lg flex-col rounded-md border-2 border-indigo-600 p-2">
-          <div className="flex-1">hhhh</div>
+          <div className="flex-1 space-y-4 overflow-y-auto p-4">
+            {messagesList.map((message, i) => {
+              const isCurrentUser = message.userName === userName;
+
+              return (
+                <div
+                  key={i}
+                  className={`flex items-start space-x-2 ${isCurrentUser ? "justify-end" : ""}`}
+                >
+                  <div className="flex flex-col">
+                    <div
+                      className={`max-w-md rounded-md p-3 text-sm wrap-break-word ${
+                        isCurrentUser
+                          ? "bg-indigo-500 text-indigo-50"
+                          : "bg-indigo-100 text-stone-700"
+                      }`}
+                    >
+                      <p>{message.message}</p>
+                    </div>
+                    <div
+                      className={`mt-1 flex items-center text-xs ${isCurrentUser ? "self-end" : ""}`}
+                    >
+                      <span className="text-gray-400">
+                        {isCurrentUser ? "You" : message.userName} &#9679; {message.time}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
           <div className="flex items-center gap-2">
             <label htmlFor="message" className="sr-only block font-medium">
               Room Id
